fc24f474f0b3ac396175948423a6ab80356e5d1a
nico1988 <410827024@qq.com>
2019-05-08 21:01:27 +0800

    propstype

--- src/page/TodoItem.js
+++ src/page/TodoItem.js
@@ -1,4 +1,5 @@
 import React,{ Component} from 'react';
+import PropTypes from 'prop-types'
 class TodoItem extends Component {
     constructor(props){
         super(props)
@@ -7,12 +8,22 @@ class TodoItem extends Component {
     render(){
         const {content} = this.props;
         return (
-            <li onClick={this.handleItemClick.bind(this)}>{content}</li>
+            <li onClick={this.handleItemClick.bind(this)}>{content}
+                <i>{this.props.name}</i>
+            </li>
         )
     }
     handleItemClick(){
         const { handleItemClick,index } = this.props; // 解构赋值
         handleItemClick(index)
     }
+}
+TodoItem.propTypes = { // 类似vue的props
+    content: PropTypes.string.isRequired,
+    handleItemClick: PropTypes.func,
+    index: PropTypes.string
+}
+TodoItem.defaultProps = {
+    name:" hello , this is not come from parent"
 }
 export default TodoItem; 
